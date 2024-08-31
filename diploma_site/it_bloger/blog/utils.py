5525284f3ce6dee31f33c34c85dd0f72cc4b76c8
sokolKyle <sokoloffKyle@mail.ru>
2024-08-31 12:52:04 +0500

    diploma_site_all

--- diploma_site/it_bloger/blog/utils.py
+++ diploma_site/it_bloger/blog/utils.py
@@ -4,7 +4,7 @@ from django.db.models import Count
 menu = [
     {'title': 'Добавить статью', 'url_name': 'add_page'},
     {'title': 'Обратная связь', 'url_name': 'contact'},
-
+    # {'title': 'О сайте', 'url_name': 'info'}
 ]
 
 
